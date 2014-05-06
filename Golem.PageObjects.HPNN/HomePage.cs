acb2b8b8a8621fd3e2bd5bb8a669cd1846a091b5
msiwiec <msiwiec@yahoo.com>
2014-05-06 15:14:50 -0600

    added HomePage page object

---
+++ Golem.PageObjects.HPNN/HomePage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoTest.Golem.Core;
+using ProtoTest.Golem.WebDriver;
+using OpenQA.Selenium;
+using ProtoTest.Golem.WebDriver.Elements;
+
+namespace Golem.PageObjects.HPNN
+{
+    public class HomePage : BasePageObject
+    {
+        public static HomePage OpenHomePage()
+        {
+            WebDriverTestBase.driver.Navigate().GoToUrl(Config.GetConfigValue("EnvUrl", "http://katie-dev.lab.hpnewsnow.com/"));
+            return new HomePage();
+        }
+
+        public override void WaitForElements()
+        {
+            // Implement me
+        }
+    }
+}
