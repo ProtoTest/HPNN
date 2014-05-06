b3db408ed2e8d70b7fc6e71ef6fb936939872db2
msiwiec <msiwiec@yahoo.com>
2014-05-06 14:27:19 -0600

    get project building part deaux

---
+++ Golem.Tests.HPNN/SmokeTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoTest.Golem.Core;
+using ProtoTest.Golem.WebDriver;
+using Golem.PageObjects.HPNN;
+using MbUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Golem.Tests.HPNN
+{
+    [TestFixture]
+    public class SmokeTest : WebDriverTestBase
+    {
+        [Test, Category("Smoke Test")]
+        public void Some_Test()
+        {
+            
+        }
+
+        [Test, Category("Smoke Test")]
+        public void Another_Test()
+        {
+
+        }
+    }
+}
