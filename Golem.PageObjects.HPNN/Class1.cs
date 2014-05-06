7f9b3fdc28bac1b0f7bc33b6457c834e131c595c
msiwiec <msiwiec@yahoo.com>
2014-05-06 14:03:14 -0600

    initial project solution structure

---
+++ Golem.PageObjects.HPNN/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Golem.PageObjects.HPNN
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.Out.WriteLine("Test");
+        }
+    }
+}
