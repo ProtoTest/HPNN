e36a434eb15dc04de011f6ebf227bee904f5ee1a
abreit <abreit@prototest.com>
2014-05-12 17:46:07 -0300

    Re-arrange tiles functionality
    -Verify 'Re-arrange tiles functionality': Verify initially no x's to
    remove tiles, enter Tile Settings screen.
    -Rearrange Tiles. Remove them, verify banner is displayed 'Drag and Drop
    to Rearrange Tiles' with Done button.
    -Click done, verify X's are gone.

--- Golem.PageObjects.HPNN/DashboardPage.cs
+++ Golem.PageObjects.HPNN/DashboardPage.cs
@@ -8,6 +8,7 @@ using ProtoTest.Golem.WebDriver;
 using OpenQA.Selenium;
 using ProtoTest.Golem.WebDriver.Elements;
 using ProtoTest.Golem.WebDriver.Elements.Types;
+using System.Collections.ObjectModel;
 
 namespace Golem.PageObjects.HPNN
 {
@@ -57,5 +58,59 @@ namespace Golem.PageObjects.HPNN
             header.WaitForElements();
             PersonalNewsTile.Verify().Visible();
         }
+
+        public List<String> ObtainDefaultTiles()
+        {
+            List<String> defalultTiles = new List<String>();
+            Element container = new Element("tiles container",By.Id("container"));
+
+            var tilesInContainer = container.FindElements(By.XPath("//div[@gridster-item='tile']/div/div/div/h2"));
+            foreach (var tile in tilesInContainer) {
+                defalultTiles.Add(tile.Text);
+            }
+                return defalultTiles;
+        }
+
+        public void verifyDefaultTilesWithoutRemoveOption(String tileTitle)
+        {
+            string xpath = String.Format("//div[ (./div[contains(@class,'tile-presentation-controller')]/div/h2[text()='{0}']) and (//a[@class='remove-btn'])]", tileTitle);
+                Element tile = new Element("Tile Ready To Remove", By.XPath(xpath));
+                var removeButton = tile.FindInChildren(By.TagName("a"));
+                tile.Verify().Visible();
+                removeButton.Verify().Not().Visible();
+            }
+        public void verifyTileWithRemoveOption(string tileTitle)
+        {
+            string xpath = String.Format("//div[ (./div[contains(@class,'tile-presentation-controller')]/div/h2[text()='{0}']) and (//a[@class='remove-btn'])]", tileTitle);
+            if (!tileTitle.Equals("Your Personal News"))
+            {
+                Element tile = new Element("Tile Ready To Remove", By.XPath(xpath));
+                var removeButton = tile.FindInChildren(By.TagName("a"));
+                //uncomment to remove the tile
+                //removeButton.Click();
+                tile.Verify().Visible();
+            }
+        }
+
+        public void verifyBannerAndDoneButton(Boolean selectOption)
+        {
+            //if select option is true, click on done button, if not, click on close button
+
+            String text = String.Format("//div[(./div/p[text()='{0}'])  and (./div/a)]", "Drag and Drop to Rearrange Tiles");
+            Element banner = new Element(By.XPath(text));
+            banner.Verify().Visible();
+            if (selectOption)
+            {
+                var buttonDone = banner.FindInChildren(By.Id("reorder-finished"));
+                buttonDone.Click();
+            }
+            else {
+                var buttonCancel = banner.FindInChildren(By.Id("reorder-canceled"));
+                buttonCancel.Click();
+            }
+        }
     }
 }
+
+
+
--- Golem.Tests.HPNN/SmokeTest.cs
+++ Golem.Tests.HPNN/SmokeTest.cs
@@ -8,22 +8,56 @@ using ProtoTest.Golem.WebDriver;
 using Golem.PageObjects.HPNN;
 using MbUnit.Framework;
 using OpenQA.Selenium;
+using System.Collections.ObjectModel;
 
 namespace Golem.Tests.HPNN
 {
     [TestFixture]
     public class SmokeTest : WebDriverTestBase
     {
-        [Test, Category("Smoke Test")]
-        public void Some_Test()
+        //[Test, Category("Smoke Test")]
+        //public void Some_Test()
+        //{
+        //    DashboardPage.OpenDashboardPage().EnterSettings().Enter_Tiles();
+        //}
+
+        //[Test, Category("Smoke Test")]
+        //public void Another_Test()
+        //{
+        //    DashboardPage.OpenDashboardPage().VerifyDefaultTilesDisplayed();
+        //}
+        [Test, Category("Re-arrange tiles functionality")]
+        public void Re_arrange_tiles()
         {
-            DashboardPage.OpenDashboardPage().EnterSettings().Enter_Tiles();
+
+// - Verify 'Re-arrange tiles functionality': Verify initially no x's to remove tiles, enter Tile Settings screen. 
+//Rearrange Tiles. Remove them, move them, verify banner is displayed 'Drag and Drop to Rearrange Tiles' with Done button. 
+//Click done, verify X's are gone.
+//- Adding/removing tiles with specific grid size, verify displayed as expected
+//- Verifying tile grid sizes (1x1, 2x2) when adding, per the HPNN+ Scope of work
+//- Verifying expected available grid sizes
+//- Verifying list of default tiles when not logged in
+//- Verify content exists in the 'Marque' carousel, verify arrows scroll.
+
+
+            DashboardPage.OpenDashboardPage();
+            DashboardPage dashboard = new DashboardPage();
+            List<String> defaultTiles = dashboard.ObtainDefaultTiles();
+            foreach (String defaultTile in defaultTiles) {
+                dashboard.verifyDefaultTilesWithoutRemoveOption(defaultTile);
+            }
+            
+            dashboard.EnterSettings().RearrangeTiles();
+            foreach (String tileTitle in defaultTiles)
+            {
+                dashboard.verifyTileWithRemoveOption(tileTitle);
+            }
+            dashboard.verifyBannerAndDoneButton(true);
+            foreach (String defaultTile in defaultTiles)
+            {
+                dashboard.verifyDefaultTilesWithoutRemoveOption(defaultTile);
+            }
         }
 
-        [Test, Category("Smoke Test")]
-        public void Another_Test()
-        {
-            DashboardPage.OpenDashboardPage().VerifyDefaultTilesDisplayed();
-        }
     }
 }
